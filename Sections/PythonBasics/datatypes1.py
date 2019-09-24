ac258fdfde205cb593d80854d4b4fd90e76c1372
Mohammed Asslahi <asslahi37@gmail.com>
2019-09-24 12:07:37 +0300

    intro to numbers

---
+++ Sections/PythonBasics/datatypes1.py
@@ -0,0 +1,17 @@
+# 1.Fundamental Data types (values)
+int 
+float
+bool
+str
+list
+tuple
+set
+dict
+
+#2.classes => custom types
+
+#3.specialized data types = packages and modulas , extensions
+
+#none  
+
+ 
---
+++ Sections/PythonBasics/numbers1.py
@@ -0,0 +1,20 @@
+# data types 
+
+#1. int #intiger
+print(2 + 4)
+print(2 - 4)
+print(2 * 4)
+print(2 / 4)
+
+ #2. floating point number
+ #number with a decimal point 
+print(type(0.0001))
+
+# power of 
+print(2**3)
+
+#double divide
+print(2//4) #returns an intiger rounded down
+
+#modulo
+print(5 % 4) # whats the remainder of this divison
