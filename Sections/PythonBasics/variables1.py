caeb112b8db9c2118422735d5d548c917bcc7dfb
Mohammed Asslahi <asslahi37@gmail.com>
2019-09-24 14:50:11 +0300

    varibales 1

---
+++ Sections/PythonBasics/variables1.py
@@ -0,0 +1,19 @@
+# variables 
+# store info that can be used 
+
+name = "Mo" #the value is called binding (bound)
+print(name)
+
+#tips
+#snack-case
+#start with a lowercase
+#letters, number and underscore
+#case sensitive
+#dont overwrite keywords
+
+
+#constant = not to change
+PI = 3.14
+
+#dunder
+__
