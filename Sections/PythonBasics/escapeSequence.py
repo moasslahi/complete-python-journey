10c44455135cb28573c59c34f75269e25b09f1b6
Mohammed Asslahi <asslahi37@gmail.com>
2019-09-27 22:07:21 +0300

    escape sequence

---
+++ Sections/PythonBasics/escapeSequence.py
@@ -0,0 +1,15 @@
+# escape sequence \
+# whats coming after this is a string python !
+
+#oldWeather = "It's "kind of" sunny"
+
+#should be 
+newWeather = "It\'s \"kind of\" sunny"
+print(newWeather)
+
+
+#\t = add a tap spacing 
+
+#\n = add a new line
+newWeather = "It\'s \"kind of\" sunny \n hope you have a good day"
+print(newWeather)
