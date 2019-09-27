5c5f01da2fcbc58103d703ab92316cb8b083062d
Mohammed Asslahi <asslahi37@gmail.com>
2019-09-27 21:50:33 +0300

    strings

---
+++ Sections/PythonBasics/strings1.py
@@ -0,0 +1,20 @@
+# strings
+# peice of text
+
+print(type("Hello"))
+
+username = "Moasslahi"
+password = "Mo123"
+
+long_strings = '''
+Wow
+Wow
+:)
+'''
+print(long_strings)
+
+first_name = 'Mo '
+last_name = 'Asslahi'
+
+full_name = first_name + last_name
+print(full_name)
