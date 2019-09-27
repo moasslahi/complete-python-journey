1f7dd2ebd6fb18707187258e88d29cfa2a0dd952
Mohammed Asslahi <asslahi37@gmail.com>
2019-09-28 00:05:08 +0300

    methods and buit in func

---
+++ Sections/PythonBasics/BuiltInFunc.py
@@ -0,0 +1,12 @@
+# built in functions and methods
+
+# length of the string
+print(len("Mo"))
+
+
+#methods
+#  . then method
+x = ' Mo Asslahi'
+print(x.upper())
+print(x.find('Mo'))
+print(x.replace('Mo','Ahmed'))
