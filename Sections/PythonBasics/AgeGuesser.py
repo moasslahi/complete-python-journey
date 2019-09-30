9464bb15d455b104b4c0a8407f43ca2835cc87d7
Mohammed Asslahi <asslahi37@gmail.com>
2019-09-30 19:16:09 +0300

    lists and lists slicing

--- Sections/PythonBasics/AgeGuesser.py
+++
@@ -1,6 +0,0 @@
-
-Birth_year = input(" What year were you born")
-
-age = 2019 - int(Birth_year)
-
-print(f'Your age is: {age}')
---
+++ Sections/PythonBasics/lists1.py
@@ -0,0 +1,21 @@
+#lists are collection of items
+#ordered seqeunce of any type
+#Data structure  = organize info into folder
+
+Mylist = [1,2,3,4,5]
+Mylist2 = ['a','b','c']
+Mylist3 = [1,2,3,'a', False]
+
+
+amazon_cart = [
+    'sunglasses',
+    'book',
+    'Iphone',
+    'Ipad',
+    'laptop'
+]
+print(amazon_cart[0])
+
+#list slicing
+new_cart = amazon_cart[:]
+print(new_cart)
