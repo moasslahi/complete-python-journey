f8528be1d48395b915958a49c3e38db03f380708
Mohammed Asslahi <asslahi37@gmail.com>
2019-10-02 10:40:15 +0300

    save

--- Sections/PythonBasics/lists1.py
+++ Sections/PythonBasics/lists1.py
@@ -1,21 +1,57 @@
-#lists are collection of items
-#ordered seqeunce of any type
-#Data structure  = organize info into folder
+# #lists are collection of items
+# #ordered seqeunce of any type
+# #Data structure  = organize info into folder
 
-Mylist = [1,2,3,4,5]
-Mylist2 = ['a','b','c']
-Mylist3 = [1,2,3,'a', False]
+# Mylist = [1,2,3,4,5]
+# Mylist2 = ['a','b','c']
+# Mylist3 = [1,2,3,'a', False]
+
+# amazon_cart = [
+#     'sunglasses',
+#     'book',
+#     'Iphone',
+#     'Ipad',
+#     'laptop'
+# ]
+# print(amazon_cart[0])
+# #-----------------------------------------------------
+# #list slicing
+# new_cart = amazon_cart[:]
+# print(new_cart)
+
+# #-------------------------------------------------------
+# #lists method = action = specific to data type
+
+# #adding
+# basket = [1,2,3,4,5]
+# basket.append(6) #adding
+# basket.insert(0,7) #insert using the index
+# basket.extend([8]) #iterable
 
 
-amazon_cart = [
-    'sunglasses',
-    'book',
-    'Iphone',
-    'Ipad',
-    'laptop'
-]
-print(amazon_cart[0])
+# #removing
+# basket.pop() #pops end of the list or specified index
+# basket.clear # clears the whole list / removes items in list
 
-#list slicing
-new_cart = amazon_cart[:]
-print(new_cart)
+
+# #index
+# print(basket.index(2))
+
+# #sort 
+# print(basket.sort()) #sorts the list
+# sorted() #prints new list 
+
+# #copy list 
+# newbasket = basket.copy
+
+# #reverse 
+# basket.reverse #switching all the indixes
+
+# #common list patterns 
+# print(len(basket))
+
+# #range
+# print(list(range(1,100)) #generate a list quickly
+
+# # .join => string method
+
