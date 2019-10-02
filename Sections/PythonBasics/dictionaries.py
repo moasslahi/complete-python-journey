ff89913ba18365f64cb715bee17bb696353e4d6d
Mohammed Asslahi <asslahi37@gmail.com>
2019-10-02 11:08:29 +0300

    dictionaries

---
+++ Sections/PythonBasics/dictionaries.py
@@ -0,0 +1,18 @@
+# Dictionary
+#data type + data structure
+#organize our data 
+#key and value pair 
+#unordered !
+
+myDict = [
+    {
+    'name': 'Mo',
+    'age': 20,
+    'number': [1,2,3]
+    },
+    {
+    'name': 'Ahmed'
+    }
+]
+
+print(myDict[1]['name'])
