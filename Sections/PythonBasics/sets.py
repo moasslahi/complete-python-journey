0759b8135d32ce8b45722816dcb6dfec26603587
Mohammed Asslahi <asslahi37@gmail.com>
2019-10-04 22:24:10 +0300

    set methods

--- Sections/PythonBasics/sets.py
+++ Sections/PythonBasics/sets.py
@@ -19,3 +19,33 @@ print(1 in my_set2)
 #len 
 print(len(my_set2))
 
+#---------------------------
+#set methods 2 
+myset = {1,2,3,4,5}
+yourset = {4,5,6,7,8,9,10}
+
+#.differnece()
+print(myset.difference(yourset))
+
+#.discard
+print(myset.discard(1))
+
+#.difference_update()
+print(myset.difference_update(yourset))
+
+#.intersection()
+print(myset.intersection(yourset))
+
+#isdisjoin()
+print(myset.isdisjoint(yourset))
+#or
+print(myset | yourset)
+
+#.union
+print(myset.union(yourset))
+
+#issubset
+print(myset.issubset(yourset))
+
+#issuperset
+print(yourset.issuperset(myset))
