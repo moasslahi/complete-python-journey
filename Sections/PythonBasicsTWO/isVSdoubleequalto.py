33cdecd812aa6d56c74a27454881ed87ec84c46f
Mohammed Asslahi <asslahi37@gmail.com>
2019-10-11 17:54:58 +0300

    is vs ==

---
+++ Sections/PythonBasicsTWO/isVSdoubleequalto.py
@@ -0,0 +1,16 @@
+# equal checks equality in value
+print(True == 1)    # this will be converteed to 
+                    #boolean value 1 is 0 
+
+print('' == 1)      # an empty string is false
+
+
+print([] == 1)      # an empty array is falsy
+
+print(10 == 10.0)   
+
+print([] == [])
+
+
+
+# 'is' checks the location of the value 
