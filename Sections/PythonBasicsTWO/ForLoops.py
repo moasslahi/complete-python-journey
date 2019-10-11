d248665cbc7086434984fea45963b9da909137b9
Mohammed Asslahi <asslahi37@gmail.com>
2019-10-11 18:51:24 +0300

    for loops

---
+++ Sections/PythonBasicsTWO/ForLoops.py
@@ -0,0 +1,17 @@
+# for loops
+# allows us to iterate through list of items
+for x in range(1,10):
+	print(x) 
+
+# iterable = loops over
+
+for item in "Mo Asslahi":
+	print(item)
+
+for item in [1,2,3,4,5]:
+	print(item)
+
+# nesting 
+for item in (1,2,3):
+	for x in ['a','b','c']:
+		print(item,x)
