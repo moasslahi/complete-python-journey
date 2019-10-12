e7742cba853ff2b9b719fc2c08bad39d32686bcf
Mohammed Asslahi <asslahi37@gmail.com>
2019-10-12 22:46:58 +0300

    range()

---
+++ Sections/PythonBasicsTWO/range.py
@@ -0,0 +1,14 @@
+# range function 
+
+for number in range(0,10):
+    print(number)
+
+for n in range(0,10,2):
+    print(n)
+
+for n in range(10,0,-1):
+    print(n)
+
+#quick way to create a list
+for i in range(2):
+    print(list(range(10)))
