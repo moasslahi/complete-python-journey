daa75ccfff555b35823f8e12296bff3a2b78f971
Mohammed Asslahi <asslahi37@gmail.com>
2019-10-22 19:01:51 +0300

    arguments and parameters

--- Sections/PythonBasicsTWO/Functions.py
+++ Sections/PythonBasicsTWO/Functions.py
@@ -11,3 +11,24 @@ def say_hello():
 say_hello()
 
 
+
+#-------------------------------------------
+
+
+# paramter are name of variables we recieve
+def say_hello(name):
+    print(f'Hello {name}')
+
+
+#argumemnt are actual values we provide to the function
+say_hello('Mo') #call , invoke
+
+
+#-----------------------------------------------------
+
+# propositional arguments require to proper postion
+
+# key word argumnets = not worry about the position 
+say_hello(name='AHMED') # its bad practice = complicated code
+
+# default argumemnts
