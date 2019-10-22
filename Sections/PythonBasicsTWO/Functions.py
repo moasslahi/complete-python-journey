528e0b87118fb295959f42f746f80f0be216824c
Mohammed Asslahi <asslahi37@gmail.com>
2019-10-22 18:24:33 +0300

    functions

---
+++ Sections/PythonBasicsTWO/Functions.py
@@ -0,0 +1,13 @@
+# functions
+# DRY
+# functions are useful when have things that you
+# want to use over and over
+
+#creating functions
+def say_hello():
+    print("Hello")
+
+# we have to call it WITH BRACKETS to run  
+say_hello()
+
+
