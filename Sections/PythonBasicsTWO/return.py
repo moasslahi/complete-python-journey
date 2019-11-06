26bd90fab30ac229ec88f0e58c86def7db1439fc
Mohammed Asslahi <asslahi37@gmail.com>
2019-11-06 10:42:56 +0300

    return in functions

---
+++ Sections/PythonBasicsTWO/return.py
@@ -0,0 +1,13 @@
+# return 
+# functions automatically returns none
+#so we must add RETURN
+#returns automatically exits the function
+
+
+def sum(num1,num2):
+    return num1 + num2
+
+# functions should do 1 thing really well
+# functions should return something
+
+print(sum(4,5))
