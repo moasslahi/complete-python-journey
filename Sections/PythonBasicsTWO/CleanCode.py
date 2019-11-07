c0d1eec481af6e73db9f42be2278f8d7623d45c0
Mohammed Asslahi <asslahi37@gmail.com>
2019-11-07 09:45:58 +0300

    clean code

---
+++ Sections/PythonBasicsTWO/CleanCode.py
@@ -0,0 +1,15 @@
+#clean code
+
+def is_even(num):
+    if num % 2 == 0:
+        return True
+    elif num % 2!=0:
+        return False
+is_even(50)
+
+
+# to clean the code even more
+def is_even(num):
+    return num % 2 == 0
+
+print(is_even(50))
