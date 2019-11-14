45be252d07a81af83a50d08c5fa4ca3553a3131a
Mohammed Asslahi <asslahi37@gmail.com>
2019-11-14 11:54:57 +0300

    intro to scope

---
+++ Sections/PythonBasicsTWO/scope.py
@@ -0,0 +1,7 @@
+# scope 
+# = what variables do i have access to 
+
+#global scope = accessed to everyone in the file
+
+#function scope = only accessed within functions. when we created a functions, it has its own world.
+
