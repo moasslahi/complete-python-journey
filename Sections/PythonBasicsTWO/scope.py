b86dadc588799a919a4d7206aa95dab0cc3834e4
Mohammed Asslahi <asslahi37@gmail.com>
2019-11-14 12:02:20 +0300

    scope order

--- Sections/PythonBasicsTWO/scope.py
+++ Sections/PythonBasicsTWO/scope.py
@@ -5,3 +5,9 @@
 
 #function scope = only accessed within functions. when we created a functions, it has its own world.
 
+
+#scope rules/ order
+#1 start with local scope
+#2 is there parent local scope?
+#3 global
+#4 built in python functions
