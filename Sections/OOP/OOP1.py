93ff2187f83d63f9237a68577f7574c68a20d634
Mohammed Asslahi <asslahi37@gmail.com>
2019-11-23 11:13:00 +0300

    intro to OOP

---
+++ Sections/OOP/OOP1.py
@@ -0,0 +1,23 @@
+# object oriented programming 1
+# ---------------------------------
+# able to creat our own datatypes with pur own attributes
+# objects have methods and attributes
+# class keyword
+# everything is an object
+# built by class keyword
+
+
+# its a paradiam
+# its a way to think and structure our code
+# break it into many objects and pieces
+# to be organized
+
+
+class BigObject:
+    pass
+
+
+obj1 = BigObject()  # instanciate
+
+# class is the blueprint, builing
+# instances are objects instantiated from class
